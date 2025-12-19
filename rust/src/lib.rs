361101ff23a456f368ff5124227a87ef4d412fae
bissli <bissli@xyz.org>
2025-12-19 10:45:21 -0500

    fix: add more Clippy allows for readable code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,9 +4,11 @@
 #![allow(clippy::redundant_pattern_matching)]
 #![allow(clippy::manual_map)]
 #![allow(clippy::collapsible_else_if)]
+#![allow(clippy::collapsible_if)]
 #![allow(clippy::nonminimal_bool)]
 #![allow(clippy::manual_swap)]
 #![allow(clippy::while_let_loop)]
+#![allow(clippy::needless_as_bytes)]
 
 mod calendar;
 mod parser;
